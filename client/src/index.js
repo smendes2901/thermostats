eb625812ba8fcce612c85c5d320228c49ce03472
Shaun Mendes <shaun.mendes@MULPDL0039.fractal.com>
2019-10-25 23:33:18 +0530

    Login css changes and redux devtools removed

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,7 @@ import thunk from 'redux-thunk'
 const store = createStore(
     rootReducer,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(thunk)
     )
 )
 
