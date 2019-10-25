68531b0db79a1c9870f33b1c9194cab63cba2ab8
smendes2901 <56862435+smendes2901@users.noreply.github.com>
2019-10-26 00:11:55 +0530

    keys updated

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
 module.exports = {
-    // mongoURI: 'mongodb+srv://shaun:shaun123@cluster0-xxig9.mongodb.net/thermostats?retryWrites=true&w=majority',
-    // mongoTableURI: 'mongodb+srv://shaun:shaun123@cluster0-xxig9.mongodb.net/tables?retryWrites=true&w=majority',
+    //default database 
+    mongoURI: 'mongodb+srv://shaun:shaun123@cluster0-xxig9.mongodb.net/thermostats?retryWrites=true&w=majority',
+    //json storage database
+    mongoTableURI: 'mongodb+srv://shaun:shaun123@cluster0-xxig9.mongodb.net/tables?retryWrites=true&w=majority',
     secretOrKey: 'secret',
-    mongoURI: 'mongodb://localhost:27017/thermostat?retryWrites=true&w=majority',
-    mongoTableURI: 'mongodb://localhost:27017/tables?retryWrites=true&w=majority',
-
 }
