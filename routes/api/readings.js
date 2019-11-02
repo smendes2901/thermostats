6d27199610c3f6aee70b4be49d805a12c3f55310
Shaun Mendes <shaun.mendes@MULPDL0039.fractal.com>
2019-11-02 23:17:49 +0530

    upload stream added

--- routes/api/readings.js
+++ routes/api/readings.js
@@ -3,125 +3,132 @@ const passport = require('passport')
 const router = express.Router()
 const fs = require('fs')
 const JSONStream = require('JSONStream')
-const {
-    epochToString,
-    stringToEpoch
-} = require('../../utils/datetime')
-const {
-    read,
-    upload
-} = require('../../utils/JSON')
-const {
-    insertAlerts
-} = require('../../utils/alerts')
-
-// Load alert model...
+const { epochToString, stringToEpoch } = require('../../utils/datetime')
+const { upload } = require('../../utils/JSON')
+const { insertAlerts } = require('../../utils/alerts')
 const Alert = require('../../models/Alert')
 
 //get file list from folder
 router.get('/', async (req, res) => {
-    const testFolder = './data/'
-    fs.readdir(testFolder, (err, files) => {
-        if (err) res.status(400).json(err.message)
-        res.send(files)
-    })
+	const testFolder = './data/'
+	fs.readdir(testFolder, (err, files) => {
+		if (err) res.status(400).json(err.message)
+		res.send(files)
+	})
 })
 
 //get selected json
-router.post('/', passport.authenticate('jwt', {
-    session: false
-}), (req, res) => {
-    try {
-        const stream = fs.createReadStream('./data/' + req.body.name, {
-            encoding: 'utf8'
-        })
-        const parser = JSONStream.parse('*');
-        const record = {}
+router.post(
+	'/',
+	passport.authenticate('jwt', {
+		session: false
+	}),
+	(req, res) => {
+		try {
+			const fileName = req.body.name
+			const stream = fs.createReadStream('./data/' + fileName, {
+				encoding: 'utf8'
+			})
+			const parser = JSONStream.parse('*')
+			const record = {}
+			const counter = {}
 
-        stream.pipe(parser);
+			stream.pipe(parser)
 
-        parser.on('data', async (obj) => {
-            const val = obj.val
-            const ts = await epochToString(obj.ts)
-            if (ts.startsWith('2015')) {
-                if (Reflect.apply({}.hasOwnProperty, record, [ts])) {
-                    record[ts] += val
-                } else {
-                    record[ts] = val
-                }
-            }
-        });
+			parser.on('data', async obj => {
+				const val = obj.val
+				const ts = await epochToString(obj.ts)
+				if (ts.startsWith('2015')) {
+					if (Reflect.apply({}.hasOwnProperty, record, [ts])) {
+						record[ts] += val
+						counter[ts] += 1
+					} else {
+						record[ts] = val
+						counter[ts] = 1
+					}
+				}
+			})
 
-        parser.on('close', async () => {
-            const records = []
-            for (const key in record) {
-                if (Reflect.apply({}.hasOwnProperty, record, [key])) {
-                    const info = {}
-                    info.x = await stringToEpoch(key)
-                    info.y = record[key]
-                    records.push(info)
-                }
-            }
-            return res.send(records)
-        })
-    } catch (err) {
-        return res.status(400).send(err)
-    }
+			parser.on('close', async () => {
+				const records = []
+				for (const key in record) {
+					if (Reflect.apply({}.hasOwnProperty, record, [key])) {
+						const info = {}
+						info.x = await stringToEpoch(key)
+						info.y = record[key] / counter[key]
+						records.push(info)
+					}
+				}
+				return res.send(records)
+			})
+		} catch (err) {
+			return res.status(400).send(err.message)
+		}
+	}
+)
 
-})
-
-const uploadAndNotify = async (data, name, req) => {
-    //get the attached socket listner instance
-    const io = req.app.get('socketio')
-    //create a message to be sent by the socket
-    let message = {}
-    try {
-        await upload(data, name)
-        message = {
-            message: name + ' Upload SuccessFul',
-            status: 'success'
-        }
-    } catch (err) {
-        message = {
-            message: err.message,
-            status: 'error'
-        }
-
-    }
-    insertAlerts(message).then(id => {
-        //update the alerts table once the upload fails/succeeds
-        Alert.findById({
-            _id: id
-        }).then(msg => {
-            //on successful update send the message via socket to client
-            io.emit('test', {
-                msg
-            })
-        }).catch(err => {
-            //console.log error message
-            console.log(err.message)
-        })
-    }).catch(err => {
-        console.log(err.message)
-    })
+const uploadAndNotify = async (io, fileName, collectionName) => {
+	const stream = fs.createReadStream('./data/' + fileName, {
+		encoding: 'utf8'
+	})
+	const parser = JSONStream.parse('*')
+	let records = []
+	stream.pipe(parser)
+	parser.on('data', async data => {
+		records.push(data)
+		if (records.length === 1000) {
+			stream.pause()
+			await upload(records, collectionName).then(() => {
+				records = []
+			})
+			stream.resume()
+		}
+	})
+	parser.on('close', async () => {
+		//update the reamaining records to the database
+		await upload(records, collectionName)
+		let message = {
+			message: fileName + ' Upload Successful',
+			status: 'success',
+			read: false
+		}
+		insertAlerts(message).then(id => {
+			//update the alerts table once the upload fails/succeeds
+			Alert.findById({
+				_id: id
+			}).then(msg => {
+				//on successful update send the message via socket to client
+				io.emit('test', {
+					msg
+				})
+			}).catch(err => {
+				//console.log error message
+				console.log(err.message)
+			})
+		}).catch(err => {
+			console.log(err.message)
+		})
+	})
 }
 
-
 //Upload json to mongo
-router.post('/upload', passport.authenticate('jwt', {
-    session: false
-}), async (req, res) => {
-    try {
-        //split file name based on '.' and extract the name to be used as collectionName
-        const collectionName = req.body.name.split('.')[0]
-        const readings = await read(req.body.name)
-        //Upload json and notify user asynchronously
-        uploadAndNotify(readings, collectionName, req)
-        //Notify user on successful initiation of upload
-        return res.status(200).send('Upload to mongo started')
-    } catch (err) {
-        return res.status(400).send(err.message)
-    }
-})
+router.post(
+	'/upload',
+	passport.authenticate('jwt', {
+		session: false
+	}),
+	async (req, res) => {
+		const io = req.app.get('socketio')
+		const fileName = req.body.name
+		const collectionName = fileName.split('.')[0]
+		fs.exists('./data/' + fileName, (result) => {
+			if (result === false) return res.status(400).send('File doesn\'t exist')
+			uploadAndNotify(io, fileName, collectionName)
+			return res.send('Upload has successfully started')
+		})
+
+
+	}
+)
 
 module.exports = router
