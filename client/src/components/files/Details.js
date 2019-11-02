282e8e4f6d87c5125ee8a9f44b04c9d60118d8b2
Shaun Mendes <shaun.mendes@MULPDL0039.fractal.com>
2019-11-02 17:48:44 +0530

    slug field added

--- client/src/components/files/Details.js
+++ client/src/components/files/Details.js
@@ -1,27 +1,29 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import { Layout, Button } from 'antd'
-import LineChart from '../graph/LineChart_zoom'
-import './Details.css'
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { Layout, Button } from "antd";
+import LineChart from "../graph/LineChart_zoom";
+import "./Details.css";
 
-const { Content } = Layout
+const { Content } = Layout;
 
 const Details = props => {
-    const handleBack = () => {
-        props.history.push('/')
-    }
-    return (
-        <div>
-            <Content>
-                <div className="back">
-                    <Button type="ghost" onClick={handleBack} >Back</Button>
-                </div>
-                <div className="chart-boundaries">
-                    <LineChart fileName={props.match.params.id} />
-                </div>
-            </Content>
+  const handleBack = () => {
+    props.history.push("/");
+  };
+  return (
+    <div>
+      <Content>
+        <div className="back">
+          <Button type="ghost" onClick={handleBack}>
+            Back
+          </Button>
         </div>
-    )
-}
+        <div className="chart-boundaries">
+          <LineChart fileName={props.match.params.slug} />
+        </div>
+      </Content>
+    </div>
+  );
+};
 
-export default withRouter(Details)
+export default withRouter(Details);
